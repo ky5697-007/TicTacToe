332cbd3eff3e4354c55da2729f6d57ce44f5347d
ky5697 -007 <ky5697@srmist.edu.in>
2026-04-20 08:43:24 +0530

    UC1 implementation

---
+++ TicTacToe.java/src/main/java/org/example/TicTacToe.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class TicTacToe {
+
+    public static void main(String[] args) {
+
+        char[][] board = new char[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+
+        System.out.println("Empty Tic-Tac-Toe Board:\n");
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
